afcd241ad2d737d61eb24394754c8d1bcc71c2d4
nicozar03 <nicobudita@gmail.com>
2022-06-20 11:52:42 -0300

    TP3 W/MIDDLEWARE

--- EJS/TP3/main.js
+++ EJS/TP3/main.js
@@ -3,23 +3,20 @@ const app = express();
 const port= 3000;
 
 function isTest(req, res, next){
+    console.log("Ejecutando Middleware");
     if(req.body.isTest){
         next();
-        
-        
     } else{
         res.status(403).send(`Lo sentimos pero no sos un usuario de tipo test, x lo que no tenes acceso a ${req.url}`);
     }
-    return req.body.isTest;
-}
+    
+}   
 app.use(express.json());
 app.use(isTest);
 
 app.get('/test', (req,res)=>{
     res.send("Sos un usuario del tipo test");
 });
-
-
 
 app.listen(port, ()=>{
     console.log(`Example Listening on port ${port}`)
