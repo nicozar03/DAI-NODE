357c513a1e6955676431dd5784859e428bef9725
nicozar03 <nicobudita@gmail.com>
2022-06-20 12:25:07 -0300

    TP3 Zarlenga-Chernijovsky W/MIDDLEWARE

--- EJS/TP3/main.js
+++ EJS/TP3/main.js
@@ -4,7 +4,7 @@ const port= 3000;
 
 function isTest(req, res, next){
     console.log("Ejecutando Middleware");
-    if(req.body.isTest){
+    if(req.body.isTest==true){
         next();
     } else{
         res.status(403).send(`Lo sentimos pero no sos un usuario de tipo test, x lo que no tenes acceso a ${req.url}`);
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(isTest);
 
 app.get('/test', (req,res)=>{
-    res.send("Sos un usuario del tipo test");
+    res.send("Sos un usuario del tipo test. Felicidades, lograste pasar la validacion");
 });
 
 app.listen(port, ()=>{
